0499c40ee2c0957661832cc0e52c466e0251976f
Armando <armandoherra369@gmail.com>
2018-01-13 12:47:54 -0600

    Added one solution to the reverse-string problem. All test passed.

--- exercises/reverse-string/index.js
+++ exercises/reverse-string/index.js
@@ -6,8 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverseString(str) {
+/*
+    To be able to reverse the string in this solution I will be converting the string into an array first by using .split('').
 
+    After I convert it into an array I can use the .reverse() function that allows me to reverse the items in an array.
+
+    And lastly I use the .join('') function to be able to return the array back to a string and return the result.
+*/
+
+function reverseString(str) {
+    const arr = str.split('');
+    arr.reverse();
+    return arr.join('');
 }
+
+/* 
+    *** BONUS: You can also write a shorthand like this:
+
+    function reverseString(str) {
+        return str.split('').reverse().join('');
+    }
+
+*/
 
 module.exports = reverseString;
